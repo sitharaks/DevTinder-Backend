50c5909f74b955c3972de4348a3ea3236cda21e6
Sithara KS <sithara.ks@hitachids.com>
2025-07-20 14:51:08 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const  app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("Hello Welcome to DevTinder Backend!");
-})
-app.use("/",(req, res) => {
-    res.send("Welcome to DevTinder Backend!");
-})
+app.use("/user",[(req, res, next) => {
+   // res.send("Hello Welcome to DevTinder Backend!");
+    next();
+},
+(req, res)=> {
+    res.send("Middleware executed");
+    console.log("Middleware executed");
+}])
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
