22fab96098a820b4f320da4eca2c9398a0034033
Sithara KS <sithara.ks@hitachids.com>
2025-07-20 15:42:29 +0530

    Err handling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ const { adminAuth } = require('../middlewares/auth');
 app.use("/admin", adminAuth)
 
 app.get("/admin/data", (req, res) => {
+    throw new Error("This is a test error");
     res.send("Hello Welcome to DevTinder Backend!");
+})
+
+app.use("/",(err,req, res, next)=> {
+    if(err){
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 app.listen(3000, () => {
