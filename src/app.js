529501d5324dfab137780a8d17cd1fb6c82b1375
Sithara KS <sithara.ks@hitachids.com>
2025-07-20 07:10:05 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const  app = express();
+
+app.use("/hello",(req, res) => {
+    res.send("Hello Welcome to DevTinder Backend!");
+})
+app.use("/",(req, res) => {
+    res.send("Welcome to DevTinder Backend!");
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
